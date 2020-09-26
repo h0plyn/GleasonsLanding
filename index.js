6308ce4f659b7bc6a892e57d5af5f964ed6e1884
h0plyn <ricky@rickyrhodes.com>
2020-09-25 20:21:21 -0400

    update onclick for section subtext

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Bongo')
+console.log('Bongo! We\'re running buddy!')
+
+function toggle() {
+    let lodging = document.querySelector('.lodging-sub-text')
+    console.log(lodging)
+    if (lodging.style.display === 'block') {
+        lodging.style.display = 'none';
+    } else {
+        lodging.style.display = 'block';
+    }
+}
