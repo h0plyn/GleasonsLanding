47b6ae756ad9b46533832a70b3619f8946fb8859
Ricky Rhodes <ricky@rickyrhodes.com>
2020-09-27 17:18:41 -0400

    build out sub-text functionality for each section and edit copy

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 console.log('Bongo! We\'re running buddy!')
 
-function toggle() {
-    let lodging = document.getElementsByClassName('lodging-sub-text')[0]
-    console.log(lodging)
-    if (lodging.style.display === 'block') {
-        lodging.style.display = 'none';
+function toggleFacilities() {
+    let facilities = document.getElementsByClassName('facilities-sub-text')[0];
+   
+    if (facilities.style.display === 'block') {
+        facilities.style.display = 'none';
     } else {
-        lodging.style.display = 'block';
+        facilities.style.display = 'block';
+    }
+}
+
+function toggleNature() {
+    let nature = document.getElementsByClassName('nature-sub-text')[0];
+
+    if (nature.style.display === 'block') {
+        nature.style.display = 'none';
+    } else {
+        nature.style.display = 'block';
+    }
+}
+
+function toggleRecreation() {
+    let recreation = document.getElementsByClassName('recreation-sub-text')[0];
+
+    if (recreation.style.display === 'block') {
+        recreation.style.display = 'none';
+    } else {
+        recreation.style.display = 'block';
     }
 }
